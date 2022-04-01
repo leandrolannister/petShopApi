22c36bfae75b14b2d96657084bf2e721392d1287
hendrix <leandrohendrix@gmail.com>
2022-04-01 20:59:20 -0300

    Small fix

--- src/api/supply/route.js
+++ src/api/supply/route.js
@@ -10,68 +10,57 @@ route.get('/', async (req,res,next) => {
   }    
 });
 
-route.post('/', async (req,res) => {
-   try{
-       const supply = new Supply(req.body);
-       await supply.store();
-       res.status(201).json({'message':'Created','status':201});
-   }catch(error){
-      res.send(
-         JSON.stringify({'status':204,message:error.message})
-      );       
-       handleError(error);
-   }
+route.post('/', async (req,res,next) => {
+  try{
+    const supply = new Supply(req.body);
+    await supply.store();
+    res.status(201);
+    res.end();
+  }catch(error){
+    next(error);      
+    handleError(error);
+  }
 });
 
-route.get('/:id', async (req,res) => {
-    try{
-       const {id} = req.params;
-       const supply = new Supply({id: id});
-       const data = await supply.show();
-
-       res.status(200).json({
-          'data':data,
-          'status':"200"
-       });
-
-    }catch(error){
-        res.send(
-            JSON.stringify({'error':error.message})
-        );
-        handleError(error);
-    }
+route.get('/:id', async (req,res,next) => {
+  try{
+    const {id} = req.params;
+    const supply = new Supply({id: id});
+    res.status(200).json(await supply.show());
+  }catch(error){
+    next(error);
+    handleError(error);
+  }
 });
 
-route.put('/:id', async (req,res, next) => {
-    try{
-      const {id} = req.params;
-      const data = Object.assign({},req.body,{id:id});
-      const supply = new Supply(data);
-      await supply.update();
-      res.status('200');
-      res.end();
-    }catch(error){
-      next(error);      
-      handleError(error);
-    }
+route.put('/:id', async (req,res,next) => {
+  try{
+    const {id} = req.params;
+    const data = Object.assign({},req.body,{id:id});
+    const supply = new Supply(data);
+    await supply.update();
+    res.status('200');
+    res.end();
+  }catch(error){
+    next(error);      
+    handleError(error);
+  }
 });
 
-route.delete('/:id', async (req,res) => {
-    try{
-      const supply = new Supply({id:req.params.id});
-      await supply.show();
-      await supply.delete();
-      res.end();
-    }catch(error){
-      res.send(
-        JSON.stringify({'error':error.message})
-      );      
-      handleError(error);
-    }
+route.delete('/:id', async (req,res,next) => {
+  try{
+    const supply = new Supply({id:req.params.id});
+    await supply.show();
+    await supply.delete();
+    res.end();
+  }catch(error){
+    next(error);
+    handleError(error);
+  }
 });
 
 function handleError(error){
-    throw new Error(error);
+   throw new Error(error);
 }
 
 module.exports = route;
